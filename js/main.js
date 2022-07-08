a13c8d1f0b1d21409ab235299767e08de2f86493
Giacomo Giannoni <giacomogiannoni7@gmail.com>
2022-07-08 17:15:26 +0200

    js non funzionante

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const imagesArray = [
+    "img/01.jpg",
+    "img/02.jpg",
+    "img/03.jpg",
+    "img/04.jpg",
+    "img/05.jpg",
+];
+let slideAttiva = 0;
+const itemsDom = document.querySelector('.items');
+for (let i = 0; i < imagesArray; length, i++) {
+    itemsDom.innerHTML += `<div class="item">
+                            <img class="img-slide src=${imagesArray[i]}"/>
+                            </div>`;
+};
+const itemList = document.getElementsByClassName('item');
+itemList[slideAttiva].classList.add('show');
+const circleList = document.querySelector('.up');
+const up = document.querySelector('.up');
+const down = document.querySelector('.down');
+up.addEventListener('click' ,
+    function () {
+        circleList[slideAttiva].classList.remove('active');
+        itemList[slideAttiva].classList.remove('show');
+        slideAttiva++;
+        up.classList.remove('hidden');
+        if (slideAttiva == itemList.lenght - 1) {
+            next.classList.add('hidden');
+        }
+    }
+);
+
+down.addEventListener('click' ,
+    function () {
+        circleList[slideAttiva].classList.remove('active');
+        itemList[slideAttiva].classList.remove('show');
+        slideAttiva++;
+        if (slideAttiva == 00) {
+            next.classList.add('hidden');
+        }
+    }
+);
